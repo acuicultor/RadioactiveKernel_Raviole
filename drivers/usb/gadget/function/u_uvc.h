cb448b9b0663f1e45779ee0f82d5b108da2346b2
acuicultor <ramoncuenca4@gmail.com>
2022-06-03 08:01:57 +0200

    Revert "FROMGIT: usb: gadget: uvc: allow changing interface name via configfs"
    This reverts commit 36aa07174b8344d8ada3005d425f05b92c8e285d.

--- drivers/usb/gadget/function/u_uvc.h
+++ drivers/usb/gadget/function/u_uvc.h
@@ -27,7 +27,6 @@ struct f_uvc_opts {
 
 	unsigned int					control_interface;
 	unsigned int					streaming_interface;
-	char						function_name[32];
 
 	/*
 	 * Control descriptors array pointers for full-/high-speed and
