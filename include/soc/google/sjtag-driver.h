2c59fc187200fa78d3fa2be79d36a9cdf67fbc78
celtare21 <celtare21@gmail.com>
2022-10-27 10:36:48 -0400

    sjtag: Stub function when not built
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- include/soc/google/sjtag-driver.h
+++ include/soc/google/sjtag-driver.h
@@ -8,6 +8,13 @@
 #ifndef INCLUDE_SOC_GOOGLE_SJTAG_DRIVER_H_
 #define INCLUDE_SOC_GOOGLE_SJTAG_DRIVER_H_
 
+#ifdef CONFIG_GS_SJTAG
 extern int sjtag_is_locked(void);
+#else
+static int sjtag_is_locked(void)
+{
+	return 0;
+}
+#endif
 
 #endif /* INCLUDE_SOC_GOOGLE_SJTAG_DRIVER_H_ */
